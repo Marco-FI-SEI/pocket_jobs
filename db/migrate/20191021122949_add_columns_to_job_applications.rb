fa91cacfc450387d2c0fb6b907c7d5c9e1f5fae0
Jeremy Amram <jeremy.j.amram@gmail.com>
2019-10-21 13:42:31 +0100

    models_and_migrations

---
+++ db/migrate/20191021122949_add_columns_to_job_applications.rb
@@ -0,0 +1,8 @@
+class AddColumnsToJobApplications < ActiveRecord::Migration[6.0]
+  def change
+    add_column :job_applications, :job_posting_id, :integer
+    add_column :job_applications, :applicant_id, :integer
+    
+  end
+end
+brew 
