38f0b35f74a73cbd003233af20739f4fe16cbcdb
mmman64 <mcnultymarco@gmail.com>
2019-10-24 16:32:44 +0100

    save

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     def create
 
     @applicant = Applicant.find_by(name: params[:applicant][:name])
-   
+
     session[:applicant_id] = @applicant.id
     redirect_to job_posting_path(current_applicant.random_unacknowledged_jobs)
 
@@ -15,8 +15,7 @@ class SessionsController < ApplicationController
 
     def destroy
         session.destroy
-
+        cookies.delete(:applicant_id)
         redirect_to new_session_path
-
     end
 end
